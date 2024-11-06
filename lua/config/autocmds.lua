a547be24786e58ea1c59e4ab4a90c877f26b4944
Eric Zhang <ericspring08@gmail.com>
2024-11-05 21:29:35 -0600

    updates

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,5 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+-- disable autoformat
+vim.cmd([[autocmd BufWritePre * :noautocmd silent! lua vim.lsp.buf.formatting_sync()]])
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,45 @@
 return {
   {
-    "rebelot/kanagawa.nvim",
+    "olimorris/onedarkpro.nvim",
+    name="onedarkpro",
     lazy = true,
     opts = {
-      theme = "dragon",
-      transparent = "true",
-    },
+      transparent=true,
+      styles = {
+        sidebars="transparent",
+        floats="transparent",
+        types = "NONE",
+        methods = "NONE",
+        numbers = "NONE",
+        strings = "NONE",
+        comments = "italic",
+        keywords = "bold,italic",
+        constants = "NONE",
+        functions = "italic",
+        operators = "NONE",
+        variables = "NONE",
+        parameters = "NONE",
+        conditionals = "italic",
+        virtual_text = "NONE",
+      }
+    }
+  },
+  {
+    "folke/tokyonight.nvim",
+    name="tokyonight",
+    lazy = true,
+    opts = {
+      transparent=true,
+      styles = {
+        sidebars="transparent",
+        floats="transparent"
+      }
+    }
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "tokyonight",
     },
   },
 }
